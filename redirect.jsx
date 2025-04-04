b39b9cad363f3911024ef2a49bc043b333440a2c
posh1001 <orifepaul30@gmail.com>
2025-04-04 07:34:14 +0100

    Updates

---
+++ redirect.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import Logo from '../assets/Images/logo.png';
+import 'bootstrap/dist/css/bootstrap.css';
+import Home from '../Pages/Home';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faHouse, faCoins, faChevronDown, faBell, faCirclePlus } from '@fortawesome/free-solid-svg-icons';
+import { useNavigate } from 'react-router-dom';
+
+const Beneficiaries = () => {
+    return (
+        <div className="">
+            /index.html   200
+        </div>
+
+    )
+}
+export default Beneficiaries;
